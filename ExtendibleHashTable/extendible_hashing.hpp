5e029538c213ab081a36a23349ef2e97f0c793f8
Ameya Daigavane <ameya.d.98@gmail.com>
2019-03-24 19:05:36 +0530

    Changed int to size_t for all operations on sizes.

--- ExtendibleHashTable/extendible_hashing.hpp
+++ ExtendibleHashTable/extendible_hashing.hpp
@@ -11,11 +11,11 @@ template <typename T>
 class HashBucket {
 
 	public:
-	int local_depth;
-	int max_slots;
+	size_t local_depth;
+	size_t max_slots;
 	std::vector <T> slots;
 
-	HashBucket(int local_depth, int max_slots){
+	HashBucket(size_t local_depth, size_t max_slots){
         this -> local_depth = local_depth;
 		this -> max_slots = max_slots;
 
@@ -49,7 +49,7 @@ class HashBucket {
 
     /* Checks if key is present in this bucket. */
     bool count(T key){
-        for(int i = 0; i < slots.size(); ++i){
+        for(size_t i = 0; i < slots.size(); ++i){
 
             /* Check slots. */
             if(slots[i] == key){
@@ -65,7 +65,7 @@ class HashBucket {
         assert(count(key));
 
         /* Check slots. */
-        for(int i = 0; i < slots.size(); ++i){
+        for(size_t i = 0; i < slots.size(); ++i){
             if(slots[i] == key){
                 slots.erase(slots.begin() + i);
                 return;
@@ -74,7 +74,7 @@ class HashBucket {
     }
 
 	/* Get the number of keys in this bucket. */
-	int size(){
+	size_t size(){
 		return slots.size();
 	}
 
@@ -88,14 +88,14 @@ class HashBucket {
 		return slots.size() == max_slots;
 	}
 
-	/* Print the contents of this bucket. */
+	/* Prsize_t the contents of this bucket. */
 	void print(){
 		if(isEmpty()){
 			std::cout << "Empty." << "\n";
             return;
 		}
 
-		for(int i = 0; i < slots.size(); ++i){
+		for(size_t i = 0; i < slots.size(); ++i){
 			std::cout << slots[i] << " ";
 		}
 		std::cout << "\n";
@@ -105,36 +105,36 @@ class HashBucket {
 template <typename T>
 class ExtendibleHashTable {
 
-    int global_depth;
-    int num_slots;
+    size_t global_depth;
+    size_t num_slots;
     std::vector<int> directory;
 	std::vector<HashBucket<T>> buckets;
 
 	public:
-	ExtendibleHashTable(int global_depth = 0, int num_slots = 3){
+	ExtendibleHashTable(size_t global_depth = 0, size_t num_slots = 3){
         this -> global_depth = global_depth;
         this -> num_slots = num_slots;
 
-        int num_buckets = 1 << global_depth;
+        size_t num_buckets = 1 << global_depth;
 		HashBucket<T> new_bucket(global_depth, num_slots);
-        for(int i = 0; i < num_buckets; ++i){
+        for(size_t i = 0; i < num_buckets; ++i){
             directory.push_back(i);
             (this -> buckets).push_back(new_bucket);
         }
 	}
 
 	/* Main hash function used. Currently identity. */
-	int hash(T x){
+	size_t hash(T x){
 		return x;
 	}
 
 	/* Get bucket index corresponding to this key. */
-	int get_bucket(T key){
+	size_t get_bucket(T key){
 		return hash(key) & ((1 << global_depth) - 1);
 	}
 
 	/* Rehash between buckets with indices. */
-	void rehash(int index1, int index2, int mask){
+	void rehash(size_t index1, size_t index2, size_t mask){
         HashBucket<T>& bucket1 = buckets[directory[index1]];
         HashBucket<T>& bucket2 = buckets[directory[index2]];
 
@@ -143,28 +143,17 @@ class ExtendibleHashTable {
 
         std::vector<T> keys;
 		T key;
-		int index;
+		size_t index;
 
 		while (!bucket1.isEmpty()){
 			key = bucket1.pop();
 			keys.push_back(key);
 		}
 
-        // std::cout << "Keys to be rehashed:";
-        // for(int i = 0; i < keys.size(); ++i){
-        //     std::cout << keys[i] << " ";
-        // }
-        // std::cout << "\n";
-        //
-        // bucket1.print();
-        // bucket2.print();
-        //
-        // print();
-
         assert(bucket1.isEmpty());
         assert(bucket2.isEmpty());
 
-		for (int i = 0; i < keys.size(); ++i){
+		for (size_t i = 0; i < keys.size(); ++i){
 			key = keys[i];
 
             /* Recompute bucket index, based on mask. */
@@ -173,8 +162,6 @@ class ExtendibleHashTable {
             } else {
                 index = index1;
             }
-
-            // buckets[directory[index]].insert(key);
 
             // cout << "Inserting key " << key << " into bucket indexed " << directory[index] << "\n";
 
@@ -185,8 +172,8 @@ class ExtendibleHashTable {
 
     /* Double directory, and increase global depth by 1. */
     void double_directory(){
-        int curr_size = directory.size();
-        for(int i = 0; i < curr_size; ++i){
+        size_t curr_size = directory.size();
+        for(size_t i = 0; i < curr_size; ++i){
             directory.push_back(directory[i]);
         }
         global_depth += 1;
@@ -194,7 +181,7 @@ class ExtendibleHashTable {
 
 	/* Insert into the hash table according to the hash function. */
 	void insert(T key){
-		int index = get_bucket(key);
+		size_t index = get_bucket(key);
         // cout << "Trying to insert " << key << " at index " << index << "\n";
 
         if (buckets[directory[index]].isFull()){
@@ -214,11 +201,11 @@ class ExtendibleHashTable {
 	}
 
     /* Split bucket with index. */
-    void split_bucket(int index){
+    void split_bucket(size_t index){
 
-        int local_depth = buckets[directory[index]].local_depth;
-        int index1 = index & ((1 << local_depth) - 1);
-        int index2 = index1 + (1 << local_depth);
+        size_t local_depth = buckets[directory[index]].local_depth;
+        size_t index1 = index & ((1 << local_depth) - 1);
+        size_t index2 = index1 + (1 << local_depth);
 
         HashBucket<T> new_bucket(local_depth, num_slots);
         buckets.push_back(new_bucket);
@@ -238,8 +225,8 @@ class ExtendibleHashTable {
         // buckets[directory[index1]].print();
         // buckets[directory[index2]].print();
 
-        /* Point to correct buckets. */
-        for(int i = index2 + (2 << local_depth); i < (1 << global_depth); i += (2 << local_depth)){
+        /* Posize_t to correct buckets. */
+        for(size_t i = index2 + (2 << local_depth); i < (1 << global_depth); i += (2 << local_depth)){
             directory[i] = directory[index2];
         };
 
@@ -247,10 +234,10 @@ class ExtendibleHashTable {
 
     /* Searches for the key in the hash table. Returns the index of the bucket if found, and -1 if not. */
     int search(T key){
-        int index = get_bucket(key);
+        size_t index = get_bucket(key);
 
         if(buckets[directory[index]].count(key)){
-            return index;
+            return (int) index;
         } else {
             return -1;
         }
@@ -260,7 +247,7 @@ class ExtendibleHashTable {
     void del(T key){
 
         /* Key must be present in the hash table. */
-        int index = search(key);
+        size_t index = search(key);
         assert(index != -1);
 
         /* Delete from corresponding bucket. */
@@ -272,20 +259,20 @@ class ExtendibleHashTable {
         return (search(key) != -1);
     }
 
-
 	/* Print the hash table. */
 	void print(){
         print_directory();
-		for(int i = 0; i < buckets.size(); ++i){
-			std::cout << "Bucket " << i << " (local depth " << buckets[i].local_depth << ") : ";
+        std::cout << "Global Depth: " << global_depth << "\n";
+		for(size_t i = 0; i < buckets.size(); ++i){
+			std::cout << "Bucket " << i << " (Local Depth " << buckets[i].local_depth << ") : ";
 			buckets[i].print();
 		}
 	}
 
-    /* Print the hash table directory. */
+    /* Prsize_t the hash table directory. */
     void print_directory(){
         std::cout << "Directory:" << "\n";
-		for(int i = 0; i < directory.size(); ++i){
+		for(size_t i = 0; i < directory.size(); ++i){
 			std::cout << "dir[" << i << "] = " << directory[i] << "\n";
 		}
 	}
